d711f1ca3211b9a12939cd9a602ac4540e224823
yopooja <poojasuthar65@gmail.com>
2017-02-11 22:31:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,12 @@
 console.log('Loaded!');
 
 // changing text
-var element = document.getElementById("main-text");
+var element = document.getElementById('main-text');
 element.innerHTML ="New Version ";
 
 //changing imagemove the 
 
-var img = document.getElementById("image");
+var img = document.getElementById('image');
 img.onclick = function(){
-img.style.marginleft = '100px';  
-    
+img.style.marginLeft = '100px';  
 };
