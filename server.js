7eb85d9d27ab1167f935be20859494cdb9e377d1
yopooja <poojasuthar65@gmail.com>
2017-02-12 00:45:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,15 +83,14 @@ function createTemplate(data){
     `;return htmlTemplate;
 } 
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 var counter = 0;
 app.get('/counter', function(req,res){
    counter = counter + 1;
    res.send(counter.toString());
-});
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName',function(req,res){
