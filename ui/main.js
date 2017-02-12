1a516266dca2ed98be0e507aba5c10d731672183
yopooja <poojasuthar65@gmail.com>
2017-02-12 11:05:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
-console.log('Loaded!');
-
-// changing text
-//var element = document.getElementById('main-text');
-//changing imagemove the 
-
-//var img = document.getElementById('image');
-//var marginLeft = 0;
-/*function moveRight(){
-    marginLeft = marginLeft +10;
-    img.style.marginLeft = marginLeft+ 'px';
-}
-img.onclick = function(){
-var interval = setInterval(moveRight,50); 
-};*/
+var button = document.getElementById("counter");
+button.onclick = function(){
+    //create a request object
+    var request = new XMLhttpRequest();
+    
+    //capture the response the and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLhttpRequest.DONE){
+            //Take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }//Not done 
+    };
+    request.open('GET','http://yopooja.imad.hasura-app.io/counter',true);
+    request.send('null');
+    //make  the variable  the correct spam
+    
+};
