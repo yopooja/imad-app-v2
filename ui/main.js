2f9e5393e42cc318d9dd8bd61c82bc05e351412d
yopooja <poojasuthar65@gmail.com>
2017-02-13 21:44:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,5 +42,5 @@ submit.onclick = function(){
 };
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-request.open('GET','http://yopooja.imad.hasura-app.io',true);
+request.open('GET','http://yopooja.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
