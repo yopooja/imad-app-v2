a0cb437061d6ee0dfc7f1cb9839a41c6d85aa1f3
yopooja <poojasuthar65@gmail.com>
2017-02-13 19:17:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/counter', function(req,res){
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     //get the name from request
    var name=req.query.name;
    names.push(name);
