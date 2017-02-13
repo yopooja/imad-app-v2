da444cfe75888164133e6b67c810f64cb0eb0ad2
yopooja <poojasuthar65@gmail.com>
2017-02-13 21:26:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,17 +19,30 @@ button.onclick = function(){
     //make  the request
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0;i<names.length;i++){
-        list += '<li>'+names[i]+'<li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+    
+    //capture the response the and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                var name=request.responseText;
+                names=JSON.parse(names);
+                var list = '';
+                for (var i=0;i<names.length;i++){
+                    list += '<li>'+names[i]+'<li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
 };
-
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+request.open('GET','http://yopooja.imad.hasura-app.io/counter',true);
+request.send(null);
