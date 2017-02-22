59f1184efc689d83fb48f0e12c157796c8ef3436
yopooja <poojasuthar65@gmail.com>
2017-02-22 16:47:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,8 +95,9 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/test-db',function (req, res){
     pool.query('SELECT * FROM test',function(err, result){
        if(err){
            res.status(500).send(err.toString());
