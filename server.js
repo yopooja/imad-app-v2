8160129b332f8b44a1e0256e9f3c29fa578a7be7
yopooja <poojasuthar65@gmail.com>
2017-03-04 15:29:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('articles/:articleName',function(req,res){
     // articleName === article-one
     // articles.[articleName]=={}
     // select * FROM article WHERE title ='article-one'
-    pool.query("SELECT * FROM article WHERE title ='" +req.params.articleName+ '"', function(err,result){
+    pool.query("SELECT * FROM article WHERE title ='" +req.params.articleName+ "'", function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }else{
